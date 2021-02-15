f59d8be5b603ae54d55e5f837d6d423fdf4de2db
Radule614 <stojanovicrade614@gmail.com>
2021-02-15 20:45:43 +0100

    basic profile page, basic main navigation

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,41 @@
+Main.otherUsers = {
+    infoTaken: false,
+    Info: {}
+}
+
 Main.Home = {
     init: function() {
         
+    },
+    getUsersInfo: function(){
+        fetch("http://localhost:3000/home/chatinfo", {
+            method: 'POST',
+            headers: {
+                'x-auth': window.localStorage.getItem("e2_chat_token")
+            },
+            body: {}
+        }).then(function(response) {
+            console.log(response);
+            return response.json();
+        }).then(function(response) {
+            Main.otherUsers.Info = response;
+            Main.otherUsers.infoTaken = true;
+            console.log(response);
+            Main.Home.displayOthers();
+            
+            Main.loadEnd();
+        }).catch(function(error) {
+            console.error(error);
+            Main.loadEnd();
+        });
+    },
+    displayOthers: function(){
+        let output = "";
+        let chatButtons = document.getElementById("chat-openers");
+        data = Main.otherUsers.Info.data;
+        data.forEach(function(user, index){
+            output += `<div class="chat-opener">${user.username}</div>`
+        });
+        chatButtons.innerHTML = output;
     }
 }
