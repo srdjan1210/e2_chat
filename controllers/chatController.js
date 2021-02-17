ad016163182f4e136f9c96bf74d2a34fb2ab78ff
srdjan1210 <businessrdjan@gmail.com>
2021-02-17 13:17:50 +0100

    Bug fixed

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -2,11 +2,11 @@ const io = require('../app');
 const { findOrCreateChatRoom } = require('../models/chatRoom');
 const { saveMessage } = require('../models/message');
 const { checkIfUserExists } = require('../models/user');
-const connected = [];
+let connected = [];
 
 const socketExists = (id) => {
-    for (let usr in connected) {
-        if (usr.id == id)
+    for(let usr in connected) {
+        if(usr.id == id)
             return true;
     }
 
@@ -14,35 +14,38 @@ const socketExists = (id) => {
 }
 
 
-
 module.exports = (io) => {
     io.on('connection', (socket) => {
         socket.on('new user', (id) => {
-            if (!socketExists(id))
+            if(!socketExists(id))
                 connected.push({ socketid: socket.id, id });
+
+           
         });
 
-        socket.on('join room', async(id, callback) => {
+        socket.on('join room', async (id, callback) => {
             const channel = connected.filter(usr => usr.id == id);
-            socket.join(channel.socketid);
-            callback(channel.socketid);
+            socket.join(channel[0].socketid);
+            callback(channel[0].socketid);
         });
 
-        socket.on('leave room', async(room) => {
+        socket.on('leave room', async (room) => {
             socket.leave(room);
         });
 
-        socket.on('message', async({ msg, from, to, room }, callback) => {
+        socket.on('message', async ({ msg, from, to, room}, callback) => {
             socket.to(room).emit('new message', { msg, from });
-            const chatroom = await findOrCreateChatRoom([from, to]);
+            const chatroom = await findOrCreateChatRoom([from , to]);
             await saveMessage({ msg, from, to, chatid: chatroom._id });
             callback();
 
-        });
+        }); 
 
-        socket.on('disconnected', () => {
-            connected = connnected.filter(user => socket.id != user.socketid);
+        socket.on('disconnect', () => {
+            console.log("disconnected");
+            connected = connected.filter(user => socket.id != user.socketid);
+            console.log(connected);
         });
     });
 
-}
+}   
