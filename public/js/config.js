374368089c7a3ed45130d1a7d04e28c3bdf8e664
srdjan1210 <businessrdjan@gmail.com>
2021-02-25 00:58:42 +0100

    domain update

--- public/js/config.js
+++ public/js/config.js
@@ -2,5 +2,5 @@ let jsConfig = {
     defaultSection: ["home", "feed"],
     defaultUrl: "#/home/feed",
     maxChatWindows: 3,
-    domainUrl: "https://still-wildwood-26564.herokuapp.com"
+    domainUrl: "http://localhost:3000"
 }
