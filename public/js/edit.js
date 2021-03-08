e1c72c19994741de5127bcb3cf3a2bcd97562a6c
Radule614 <stojanovicrade614@gmail.com>
2021-03-08 15:12:01 +0100

    aa

--- public/js/edit.js
+++ public/js/edit.js
@@ -20,7 +20,7 @@ Main.Edit = {
             edits.forEach((edit, i) => {
                 let input = edit.querySelector("input");
                 input.value = "";
-                if (edit.classList.contains("editing")) {
+                if (edit.classList.contains("editing") && input.getAttribute("name") != "oldPassword") {
                     edit.classList.remove("editing");
                     input.setAttribute("readonly", "");
                 }
@@ -68,12 +68,12 @@ Main.Edit = {
         let Info = Main.User.Info;
         if (Info.firstname && Info.firstname.trim() == Data.firstname) Data.firstname = null;
         if (Info.lastname && Info.lastname.trim() == Data.lastname) Data.lastname = null;
-        if (Info.username && Info.username.trim() == Data.username) Data.username = null;
+        if ((Info.username && Info.username.trim() == Data.username) || (Data.username > 15 && Data.username < 4)) Data.username = null;
         if (Info.birthday && Info.birthday.trim() == Data.birthday) Data.birthday = null;
         if (Info.nationality && Info.nationality.trim() == Data.nationality) Data.nationality = null;
         if (Info.gender && Info.gender.trim() == Data.gender) Data.gender = null;
 
-        if (Info.email && Info.email.trim() == Data.email) Data.email = null;
+        if ((Info.email && Info.email.trim() == Data.email) || !Utility.checkEmail(Data.email)) Data.email = null;
         if (Info.street && Info.street.trim() == Data.street) Data.street = null;
         if (Info.country && Info.country.trim() == Data.country) Data.country = null;
         if (Info.province && Info.province.trim() == Data.province) Data.province = null;
@@ -81,40 +81,85 @@ Main.Edit = {
 
         if (!Data.image) Data.image = null;
 
-        if (Data.password == "" || Data.password != confirm) Data.password = null;
+        if (Data.newPassword == "" || Data.newPassword != Data.newPassword || (Data.newPassword > 20 && Data.newPassword < 8)) Data.newPassword = null;
     },
     parseData: function(Data) {
-        let Info = Main.User.Info;
-        let emailData = new FormData();
+        let emailData = {};
+        let passwordData = {};
+        let usernameData = {};
         if (Data.email != null) {
-            emailData.append("email", Data.email);
+            emailData["email"] = Data.email;
             Main.Edit.sendEmailData(emailData);
         }
+        if (Data.newPassword != null) {
+            passwordData["password"] = Data.oldPassword;
+            passwordData["new_password"] = Data.newPassword;
+            Main.Edit.sendPasswordData(passwordData);
+        }
+        if (Data.username != null) {
+            usernameData["new_username"] = Data.username;
+            Main.Edit.sendUsernameData(usernameData);
+        }
 
-        // formData.append("firstname", firstname);
-        // formData.append("lastname", lastname);
-        // formData.append("username", username);
-        // formData.append("birthday", birthday);
-        // formData.append("nationality", nationality);
-        // formData.append("gender", gender);
-        // formData.append("email", email);
-        // formData.append("street", street);
-        // formData.append("country", country);
-        // formData.append("province", province);
-        // formData.append("city", city);
-        // formData.append("image", image);
-        // formData.append("password", password);
-        // return formData;
+        let formData = new FormData();
+        formData.append("firstname", Data.firstname);
+        formData.append("lastname", Data.lastname);
+        formData.append("birthday", Data.birthday);
+        formData.append("nationality", Data.nationality);
+        formData.append("gender", Data.gender);
+        formData.append("street", Data.street);
+        formData.append("country", Data.country);
+        formData.append("province", Data.province);
+        formData.append("city", Data.city);
+        formData.append("image", Data.image);
+        Main.Edit.sendData(formData);
     },
     sendEmailData: function(Data) {
-        const token = window.localStorage.getItem("e2_chat_token");
-        console.log("email", Array.from(Data));
+        console.log("email", Data);
         fetch(`${jsConfig.domainUrl}/user/edit/email`, {
             method: 'POST',
             headers: {
-                'x-auth': token
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'x-auth': window.localStorage.getItem("e2_chat_token")
             },
-            body: Data
+            body: JSON.stringify(Data)
+        }).then((response) => {
+            return response.json();
+        }).then((response) => {
+            console.log(response);
+        }).catch((error) => {
+            console.error(error);
+        });
+    },
+    sendPasswordData: function(Data) {
+        console.log("password", Data);
+        fetch(`${jsConfig.domainUrl}/user/edit/password`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'x-auth': window.localStorage.getItem("e2_chat_token")
+            },
+            body: JSON.stringify(Data)
+        }).then((response) => {
+            return response.json();
+        }).then((response) => {
+            console.log(response);
+        }).catch((error) => {
+            console.error(error);
+        });
+    },
+    sendUsernameData: function(Data) {
+        console.log("username", Data);
+        fetch(`${jsConfig.domainUrl}/user/edit/username`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'x-auth': window.localStorage.getItem("e2_chat_token")
+            },
+            body: JSON.stringify(Data)
         }).then((response) => {
             return response.json();
         }).then((response) => {
@@ -124,18 +169,19 @@ Main.Edit = {
         });
     },
     sendData: function(formData) {
-        //Main.loadStart();
+        console.log(Array.from(formData));
         fetch(`${jsConfig.domainUrl}/user/edit`, {
             method: 'POST',
+            headers: {
+                'x-auth': window.localStorage.getItem("e2_chat_token")
+            },
             body: formData
         }).then((response) => {
             return response.json();
         }).then((response) => {
             console.log(response);
-            //Main.loadEnd();
         }).catch((error) => {
             console.error(error);
-            //Main.loadEnd();
         });
     }
 }
