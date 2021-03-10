fc82ba4044ab3ea77043a10c205b5a96b37ac8bc
srdjan1210 <businessrdjan@gmail.com>
2021-03-10 15:25:47 +0100

    bug fixed

--- middleware/imageUpload.js
+++ middleware/imageUpload.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 var storage = multer.diskStorage({ 
     destination: path.join(__dirname, '../public/uploads/imgs/'),
     filename: function (req, file, cb) {
-      cb(null,  req.body.username || req.payload.username + '.png');
+      cb(null, (req.body.username || req.payload.username) + '.png');
     }
 });
 
